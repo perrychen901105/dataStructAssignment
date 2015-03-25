78d1b8a228f6ea3e15c3c587b705ed7ef11c2cad
Perry <chenzhipeng901105@gmail.com>
2015-03-25 09:46:48 +0800

    add lesson 1 code

---
+++ Lesson1/AssignmentOne/AssignmentOne/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  AssignmentOne
+//
+//  Created by chenzhipeng on 3/25/15.
+//  Copyright (c) 2015 chenzhipeng. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
